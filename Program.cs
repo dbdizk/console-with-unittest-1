12b93273df26b3ba705cbf0090ca26e14c508a58
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-19 10:21:34 +0300

    application is ready

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("5 plus 6 = "+Sum(5,6));
+
+int Sum(int a, int b){
+    return a+b;
+}
